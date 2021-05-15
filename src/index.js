8143479503e0e1c35c4eb7980e3768d8cba94ed4
Serhii Unhurian <unguryan23@gmail.com>
2021-05-15 17:55:10 +0300

    feat: add multiple reducers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import configureStore from './store/configureStore';
-import * as actions from './store/bugs';
+import { bugAdded, bugRemoved, bugResolved } from './store/bugs';
+import { projectAdded } from './store/projects';
 
 const store = configureStore();
 
@@ -7,8 +8,10 @@ const unsubscribe = store.subscribe(() => {
   console.log('Store changed', store.getState());
 });
 
-store.dispatch(actions.bugAdded({ description: 'Bug 1' }));
-store.dispatch(actions.bugAdded({ description: 'Bug 2' }));
-store.dispatch(actions.bugAdded({ description: 'Bug 3' }));
-store.dispatch(actions.bugResolved({ id: 1 }));
-store.dispatch(actions.bugRemoved({ id: 3 }));
+store.dispatch(projectAdded({ name: 'Project 1' }));
+
+store.dispatch(bugAdded({ description: 'Bug 1' }));
+store.dispatch(bugAdded({ description: 'Bug 2' }));
+store.dispatch(bugAdded({ description: 'Bug 3' }));
+store.dispatch(bugResolved({ id: 1 }));
+store.dispatch(bugRemoved({ id: 3 }));

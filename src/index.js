0106eb94c07099fde7c3866e7b65386df2b395fd
Serhii Unhurian <unguryan23@gmail.com>
2021-05-15 20:32:05 +0300

    feat: add logger middleware

--- src/index.js
+++ src/index.js
@@ -17,19 +17,19 @@ const unsubscribe = store.subscribe(() => {
 });
 
 store.dispatch(userAdded({ name: 'John' }));
-store.dispatch(projectAdded({ name: 'Project 1' }));
+// store.dispatch(projectAdded({ name: 'Project 1' }));
 
-store.dispatch(bugAdded({ description: 'Bug 1' }));
-store.dispatch(bugAdded({ description: 'Bug 2' }));
-store.dispatch(bugAdded({ description: 'Bug 3' }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
-store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
-store.dispatch(bugResolved({ id: 1 }));
-store.dispatch(bugRemoved({ id: 3 }));
+// store.dispatch(bugAdded({ description: 'Bug 1' }));
+// store.dispatch(bugAdded({ description: 'Bug 2' }));
+// store.dispatch(bugAdded({ description: 'Bug 3' }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugAssignedToUser({ bugId: 2, userId: 1 }));
+// store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(bugRemoved({ id: 3 }));
 
-const x = getUnresolvedBugs(store.getState());
-const y = getUnresolvedBugs(store.getState());
-console.log(x === y); // Get the same array
+// const x = getUnresolvedBugs(store.getState());
+// const y = getUnresolvedBugs(store.getState());
+// console.log(x === y); // Get the same array
 
-const bugs = getBugsByUser(1)(store.getState());
-console.log(bugs);
+// const bugs = getBugsByUser(1)(store.getState());
+// console.log(bugs);

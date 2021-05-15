976dad448f8e0d03586be9dd461be31a756eaa86
Serhii Unhurian <unguryan23@gmail.com>
2021-05-15 21:21:35 +0300

    feat: add toast middleware

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import configureStore from './store/configureStore';
 
 const store = configureStore();
 
+store.dispatch({
+  type: 'error',
+  payload: {
+    message: 'An error occurred',
+  },
+});
+
 store.dispatch((dispatch, getState) => {
   // Call an API if the data is abscent in the store
   // When the promise is resolved -> dispatch(...)
